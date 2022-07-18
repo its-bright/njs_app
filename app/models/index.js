1a1317f47e884b54759b4e434f06b26113971abf
Bright Me <itspal2000@gmail.com>
2022-07-18 18:48:00 -0500

    added comments

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,10 @@
 const dbConfig = require("../config/db.config.js");
 
+//Reading the db password from secrets expoosed as env in the task
+//USER_NAME = process.env.DB_USERNAME;
+//PASSWORD = process.env.DB_PASSWORD;
+//const sequelize = new Sequelize(dbConfig.DB, USER_NAME, PASSWORD, {
+
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
